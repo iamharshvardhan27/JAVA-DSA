77834c78a4ecab97ab54db13f3c8a51ac3e975a8
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2024-10-11 00:43:48 +0530

    Add files via upload

---
+++ Lectures/lec2Output.java
@@ -0,0 +1,15 @@
+public class lec2Output {
+    public static void main(String[] args) {
+        //TYPES OF OUTPUT - 
+        // 1.print()
+        // 2.println()
+        // 3.\n
+        System.out.print("Hello World ");
+        System.out.println("hello world");
+        System.out.print("hello\nworld");
+
+        //PRINTING THE PATTERN
+        System.err.println();
+        System.out.println("\n****\n***\n**\n*");
+    }
+}
