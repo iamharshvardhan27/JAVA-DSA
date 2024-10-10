391074de345ab73fab5a84305e701def6eeeb399
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2024-10-10 10:17:00 +0530

    Create Lec3_1Sum.java

---
+++ Problems/Lec3_1Sum.java
@@ -0,0 +1,9 @@
+public class Lec3_1Sum {
+    public static void main(String[] args) {
+        int sum;
+        int a = 10;
+        int b = 20;
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
