69fde0c0171e4d812361e5afb4f93aacfeecc270
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2024-10-13 13:51:54 +0530

    Add files via upload

---
+++ Lectures/lec3Variables.java
@@ -0,0 +1,17 @@
+public class lec3Variables {
+    public static void main(String[] args) {
+        // VARIABLES AND DATA TYPES
+        int a = 292;
+        System.out.println(a);
+        float b = 2.3f;
+        System.out.println(b);
+        char chare = 'a';
+        System.out.println(chare);
+        boolean ispass = true;
+        System.out.println(ispass);
+        byte one = 127;
+        System.out.println(one);
+
+    }    
+}
+ 
