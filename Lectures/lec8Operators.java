bf7eb12a1507d7b6b30ce35b521413c7b7b7f949
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2024-10-24 18:17:34 +0530

    Add files via upload

---
+++ Lectures/lec8Operators.java
@@ -0,0 +1,91 @@
+public class lec8Operators {
+    public static void main(String[] args) {
+        
+        /* 
+        // Arithmetic Operators
+        int a = 10; 
+        int b = 5;
+        System.out.println("Add : " + ( a + b ));
+        System.out.println("Sub : " + ( a - b ));
+        System.out.println("Mult : " + ( a * b ));
+        System.out.println("Div : " + ( a / b ));
+        System.out.println("Modulo ( remainder ) : " + ( a % b ));
+        */
+
+
+        /* 
+        // Unary Operator
+            1.Preincreament 
+            int a = 10;
+            int b = ++a;
+            System.out.println(a);
+            System.out.println(b); 
+            11 11
+
+            2.Postincrement 
+            int a = 10;
+            int b = a++;
+            System.out.println(a);
+            System.out.println(b);
+            11 10
+
+            1.Predecrement
+            int a = 10; 
+            int b = --a;
+            System.out.println(a);
+            System.out.println(b);
+            9 9
+
+            2.Postdecrement 
+            int a = 10;
+            int b = a--;
+            System.out.println(a);
+            System.out.println(b);
+            9 10
+        */
+
+
+
+
+        /* 
+        // Relational Operators 
+        int a = 10;
+        int b = 5;
+        System.out.println(a==b); // false
+        System.out.println(a!=b); // true
+        System.out.println(a>b); // true
+        System.out.println(a<b); // false
+        System.out.println(a>=b); // true
+        System.out.println(a<=b); // false
+        */
+
+
+        /* 
+        // Logical Operators
+        System.out.println( ( 5>1 ) && ( 2>1 ) ); // true when both are true
+        System.out.println( ( 5<1 ) || ( 2<1 ) ); // false when both are false
+        System.out.println( !( 5>1 ) ); // makes statements negate
+        */
+
+
+
+        /* 
+        // Asssignment Operator
+        int a = 10; //assigning right to left
+        System.out.println(a);
+
+        int b = 10;
+        System.out.println( b += 10 ); // b = b + 10
+
+        int c = 10;
+        System.out.println( c -= 10 ); // b = b - 10
+
+        int d = 10;
+        System.out.println( d *= 10 );  // b = b * 10
+
+        int e = 10;
+        System.out.println( e /= 10 );  // b = b / 10
+        */
+
+    }    
+}
