2642d3c0c156bdea34260c286218a3b9ab51dce0
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2025-02-16 21:30:11 +0530

    Add files via upload

---
+++ Problems/lec10_4Pattern.java
@@ -0,0 +1,18 @@
+public class lec10_4Pattern {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1 ; i <= 4 ; i++){
+            System.out.println("* * * *");
+        }
+
+        System.out.println();
+
+        int counter = 1;
+        while (counter<=n) {
+            System.out.println("* * * *");
+            counter++;
+        }
+    }    
+}
+
+
